be795b132777c828a584f7a78b3f73666bd5a971
Escher Nadeau <escher.nadeau@gmail.com>
2025-10-28 12:19:56 -0400

    Add 'mode' property to gameOptions so correct mode service is loaded

--- src/components/SettingsScreen.vue
+++ src/components/SettingsScreen.vue
@@ -1897,6 +1897,7 @@ const updateRoomCode = () => {
           ...props.mode,
           modeSettings: modeSettings.value,
           gameOptions: {
+            mode: props.mode.id,
             modeSettings: modeSettings.value,
             startingItems,
             goalQueue: [],
